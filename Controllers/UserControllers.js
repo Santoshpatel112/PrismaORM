1fdc42b0800626f36bdc546e23945103571c74c3
Santosh Patel <santoshpatelvns5@gmail.com>
2025-11-09 02:03:09 +0530

    UserController

---
+++ Controllers/UserControllers.js
@@ -0,0 +1,28 @@
+import User from '../prisma/schema.prisma'
+import Prisma from '../DB/db.config.js';
+import bcrypt from 'bcryptjs';
+export const createUser= async (req,res)=>{
+    try {
+        const {email,name,password}=req.body;
+        if(!email || !password){
+            return res.json({error:"Email and Password are required"},{status:400});
+        }
+        const exitinguser=await Prisma.user.findunique({
+            where:{email : email}
+        });
+        if(exitinguser){
+            return res.json({error:"User already exists"},{status:400});
+        }
+        const newUser=await User.create({
+            data:{
+                email,
+                name,
+                password
+            }
+        });
+        return res.json(newUser);
+    } catch (error) {
+        console.error("Error creating user:", error);
+        return res.json({error:"Internal Server Error"},{status:500});
+    }
+}
